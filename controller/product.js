3a1470d4eaa175101d31ca12c680f7f8be44ef0b
thao02122002 <vant16039@gmail.com>
2022-03-12 11:18:01 +0700

    mogooes

--- controller/product.js
+++ controller/product.js
@@ -1,25 +1,55 @@
-const product = [
-        {id: 1, name: "Product A"},
-        {id: 2, name: "Product B"},
-    ];
+// const product = [
+//         {id: 1, name: "Product A"},//item
+//         {id: 2, name: "Product B"},//item
+//     ];
+import mongoose from "mongoose";
+ //1 khởi tạo model
+ const Product = mongoose.model('Product', {name: String});
 
-export const list = (req, res) => {
+
+//Danh sách sp
+export const list = async (req, res) => {
     
-    res.json(product);
+    // res.json(product);
+
+    try {
+        const products = await Product.find();
+        res.json(products);
+        
+    } catch (error) {
+        res.status(400).json({
+            message: "Lỗi k tìm thấy sản phẩm"
+        })
+    }
 }
 export const read = (req, res) => {
     
     res.json(product.find(item => item.id === +req.params.id));
 }
-export const create = (req, res) => {
-    console.log(req.body); // req lấy về 1 cái object đã thêm vào ở body
+
+//Thêm sp
+export const create = async (req, res) => {
+    // console.log(req.body); // req lấy về 1 cái object đã thêm vào ở body
    
-    product.push(req.body);// push vào product là cái mảng cũ 
-    res.json(product);
+    // product.push(req.body);// push vào product là cái mảng cũ 
+    // res.json(product);
+    try {
+        const product = await new Product(req.body).save();
+        res.json(product);
+        
+    } catch (error) {
+        res.status(400).json({
+            message: "Không thêm đc sp"
+        })
+    }
 }
+
+//Xóa sp
 export const remove = (req, res) => {
     res.json(product.filter(item => item.id !== +req.params.id));
 }
+
+
 export const update = (req, res) => {
     res.json(product.map(item => item.id == req.params.id ? req.body : item));
 }
